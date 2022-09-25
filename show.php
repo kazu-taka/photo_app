a22caa4f2bc4528d720868b6e818bfd126fb775f
kazu-taka <k.takahashi@nowall.co.jp>
2022-09-25 22:59:43 +0900

    04.mdまで終了

---
+++ show.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+<?php include_once __DIR__ . '/_head.html' ?>
+<body>
+    <?php include_once __DIR__ . '/_header.html' ?>
+
+    <main class="main_content content_center wrapper">
+        <div class="content">
+            <img src="https://picsum.photos/400/600">
+            <p>
+                画像の説明文
+            </p>
+            <div class="button">
+                <a href="edit.php" class="edit_button">編 集</a>
+                <button class="delete_button">削 除</button>
+            </div>
+        </div>
+    </main>
+
+    <?php include_once __DIR__ . '/_footer.html' ?>
+</body>
+</html>
