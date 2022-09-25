ae4f36e19f2b22d7157b7d6fc258d7a3d1551a21
kazu-taka <k.takahashi@nowall.co.jp>
2022-09-25 22:25:57 +0900

    03.mdまで終了

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="ja">
+<?php include_once __DIR__ . '/_head.html' ?>
+<body>
+    <?php include_once __DIR__ . '/_header.html' ?>
+
+    <main class="article wrapper">
+        <div class="grid" data-masonry='{"columnWidth": 270, "itemSelector": ".grid_item", "isFitWidth": true}'>
+            <div class="grid_item">
+                <a href="show.php"><img src="https://picsum.photos/200/300" height="200"></a>
+            </div>
+            <div class="grid_item">
+                <a href="show.php"><img src="https://picsum.photos/200/300" height="300"></a>
+            </div>
+            <div class="grid_item">
+                <a href="show.php"><img src="https://picsum.photos/200/300" height="250"></a>
+            </div>
+            <div class="grid_item">
+                <a href="show.php"><img src="https://picsum.photos/200/300" height="300"></a>
+            </div>
+            <div class="grid_item">
+                <a href="show.php"><img src="https://picsum.photos/200/300" height="250"></a>
+            </div>
+            <div class="grid_item">
+                <a href="show.php"><img src="https://picsum.photos/200/300" height="200"></a>
+            </div>
+            <div class="grid_item">
+                <a href="show.php"><img src="https://picsum.photos/200/300" height="250"></a>
+            </div>
+            <div class="grid_item">
+                <a href="show.php"><img src="https://picsum.photos/200/300" height="300"></a>
+            </div>
+            <div class="grid_item">
+                <a href="show.php"><img src="https://picsum.photos/200/300" height="350"></a>
+            </div>
+            <div class="grid_item">
+                <a href="show.php"><img src="https://picsum.photos/200/300" height="300"></a>
+            </div>
+            <div class="grid_item">
+                <a href="show.php"><img src="https://picsum.photos/200/300" height="200"></a>
+            </div>
+        </div>
+        <a href="upload.php" class="add_button">
+            <i class="fa-solid fa-plus"></i>
+        </a>
+    </main>
+    
+    <?php include_once __DIR__ . '/_footer.html' ?>
+    <script src="https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js"></script>
+</body>
+</html>
