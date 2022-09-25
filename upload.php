29206934df42941b10fb4cd4de51df15aaf5eb1f
kazu-taka <k.takahashi@nowall.co.jp>
2022-09-25 21:48:19 +0900

    02.mdまで終了

---
+++ upload.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <?php include_once __DIR__ . '/_head.html' ?>
+<body>
+    <?php include_once __DIR__ . '/_header.html'?>
+    <main class="upload_content content_center wrapper">
+        <form action="" method="post" class="upload_content_form" enctype="multipart/form-data">
+            <label class="upload_content_label" for="file_upload">
+                <span class="plus_icon"><i class="fas fa-plus-circle"></i></span>
+                <span class="upload_text">写真を追加</span>
+            </label>
+            <input class="input_file" type="file" id="file_upload" name="image">
+            <textarea class="input_text" name="description" rows="5" placeholder="画像の詳細を入力してください"></textarea>
+            <input class="upload_submit" type="submit" value="追加">
+        </form>
+    </main>
+    <?php include_once __DIR__ . '/_footer.html'?>
+</body>
+</html>
