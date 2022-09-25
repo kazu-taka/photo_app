47face8e8f6c096778314fc5c870b5934088c08e
kazu-taka <k.takahashi@nowall.co.jp>
2022-09-26 06:56:32 +0900

    07.mdまで終了

--- users/login.php
+++ users/login.php
@@ -5,18 +5,16 @@
     <?php include_once __DIR__ . '/../common/_header.html' ?>
 
     <main class="content_center wrapper">
-        <div class="signup_content">
-            <h2 class="signup_title">新規ユーザー登録</h1>
-            <form class="signup_form" action="" method="post">
+        <div class="login_content">
+            <h1 class="login_title">ログイン</h1>
+            <form class="login_form" action="" method="post">
                 <label class="email_label" for="email">メールアドレス</label>
                 <input type="email" name="email" id="email" placeholder="Email">
-                <label class="name_label" for="name">ユーザー名</label>
-                <input type="text" name="name" id="name" placeholder="UserName">
                 <label class="password_label" for="password">パスワード</label>
                 <input type="password" name="password" id="password" placeholder="Password">
                 <div class="button_area">
-                    <input type="submit" value="新規登録" class="signup_button">
-                    <a href="login.php" class="login_page_button">ログインはこちら</a>
+                    <input type="submit" value="ログイン" class="login_button">
+                    <a href="signup.php" class="signup_page_button">新規ユーザー登録</a>
                 </div>
             </form>
         </div>
